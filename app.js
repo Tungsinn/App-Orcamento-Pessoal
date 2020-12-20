4da8d00d4fb2f6dbeb49fa3bbe0e44dcd1ee980e
Tungsinn <gvnalamas@tutanota.com>
2020-12-20 16:37:18 -0300

    Impressão dinâmica das despesas em linhas e colunas

--- app.js
+++ app.js
@@ -123,5 +123,31 @@ function carregaListaDespesas() {
     let despesas = Array()
     // Recebe a lista com todas as despesas
     despesas = bd.recuperarTodosRegistros()
-    console.log(despesas)
+    // Seleciona o elemento tbody da tabela
+    let listaDespesas = document.getElementById('listaDespesas')
+    // Percorrer o array e listar as despesas dinamicamente
+    despesas.forEach(function(d) {
+        // Cria a linha (tr)
+        let linha = listaDespesas.insertRow()
+        //Cria as colunas (td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+        // Ajustar o tipo
+        switch(d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }
+
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
